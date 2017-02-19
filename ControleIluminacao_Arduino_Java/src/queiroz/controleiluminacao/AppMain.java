026eed3834edd9b5559843df3b3f76d92c39b5d7
User <User@MTD002>
2017-02-19 00:37:58 -0400

    teste

--- ControleIluminacao_Arduino_Java/src/queiroz/controleiluminacao/AppMain.java
+++ ControleIluminacao_Arduino_Java/src/queiroz/controleiluminacao/AppMain.java
@@ -15,6 +15,8 @@ public class AppMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.print("teste");
         // TODO code application logic here
     }
     
